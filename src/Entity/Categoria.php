9f08f0c44a7583990a0d8f9b3b388ababb8bebad
Antonio Alonso <antoal898@gmail.com>
2021-06-22 14:50:48 +0100

    First commit

---
+++ src/Entity/Categoria.php
@@ -0,0 +1,54 @@
+<?php
+// src/Entity/Categoria.php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+/**
+ * @ORM\Entity @ORM\Table(name="categorias")
+ */
+class Categoria {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer",name="CodCat")
+     */
+    private $codCat;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $nombre;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $descripcion;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $urlimg;
+    /**
+     * Bidireccional
+	* @ORM\OneToMany(targetEntity="Producto", mappedBy="categoria")
+	*/
+    private $productos;
+    public function getCodCat() {
+        return $this->codCat;
+    }
+    public function getProductos() {
+        return $this->productos;
+    }
+    public function getNombre() {
+        return $this->nombre;
+    }
+    public function getUrlImg() {
+        return $this->urlimg;
+    }
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+}
